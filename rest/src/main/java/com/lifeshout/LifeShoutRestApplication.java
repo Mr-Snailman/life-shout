41401f180ca8660374ef061081c7e9269a0e31ff
Dustin Saunders <dsaunders2414@gmail.com>
2018-07-27 00:46:52 -0400

    Added main rest application.

---
+++ rest/src/main/java/com/lifeshout/LifeShoutRestApplication.java
@@ -0,0 +1,19 @@
+package com.lifeshout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application.
+ */
+@SpringBootApplication
+public class LifeShoutRestApplication {
+
+  /**
+   * Main execution method
+   * @param args Command Line Arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(LifeShoutRestApplication.class, args);
+  }
+}
